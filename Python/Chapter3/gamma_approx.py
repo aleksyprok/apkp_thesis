03f8b4ec0d476c6865ad14704df8939c7fc5f8d0
aleksyprok <aleksyprok@gmail.com>
2021-02-04 13:36:28 +0000

    a

--- Python/Chapter3/gamma_approx.py
+++ Python/Chapter3/gamma_approx.py
@@ -5,7 +5,7 @@ N = 100
 alpha = 1
 omega = np.pi * 1e-2
 eta = 1e1
-a = 1e5
+a = 1e6
 
 gamma = omega * (4 * eta / (3 * omega * a ** 2)) ** (1/3) \
 	  * (3 * N ** (2 / 3) / 2 + zeta(1/3)) / (np.log(N) + np.euler_gamma)
